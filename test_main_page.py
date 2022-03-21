9a120f3ad7b19cb0ee2cfcdd561a621d6aaed9be
tester911007 <vladmvm2020@gmail.com>
2022-03-21 18:33:06 +0300

    Переходы между страницами

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,14 @@
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
-    # открываем страницу
     page.open()
-    # выполняем метод страницы — переходим на страницу логина
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
