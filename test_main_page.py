c74b7db9ba454946e0bc299891f53a1e30bc7da9
tester911007 <vladmvm2020@gmail.com>
2022-03-21 16:18:23 +0300

    Первый тест на основе Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,17 +1,14 @@
-from selenium.webdriver.common.by import By
-
-
-link = "http://selenium1py.pythonanywhere.com/"
-
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, link)
+    # открываем страницу
+    page.open()
+    # выполняем метод страницы — переходим на страницу логина
+    page.go_to_login_page()
 
 
 '''
