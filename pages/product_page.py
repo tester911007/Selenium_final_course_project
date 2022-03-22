4e067cc980b5eb0165bf2faeaa2f1dd972042095
tester911007 <vladmvm2020@gmail.com>
2022-03-22 16:49:00 +0300

    Задание: независимость контента, ищем баг

--- pages/product_page.py
+++ pages/product_page.py
@@ -11,15 +11,15 @@ class ProductPage(BasePage):
     def should_be_message_about_adding(self):
         assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), (
             "Product name is not presented")
-        assert self.is_element_present(*ProductPageLocators.MESSAGE_ABOUT_ADDING), (
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME_IN_MESSAGE), (
             "Message about adding is not presented")
 
         product_name = self.browser.find_element(
             *ProductPageLocators.PRODUCT_NAME).text
-        message = self.browser.find_element(
-            *ProductPageLocators.MESSAGE_ABOUT_ADDING).text
-
-        assert product_name in message, "No product name in the message"
+            
+        product_name_in_message = self.browser.find_element(
+            *ProductPageLocators.PRODUCT_NAME_IN_MESSAGE).text
+        assert product_name == product_name_in_message, "Product name does not match the message"
 
     def should_be_message_basket_total(self):
         assert self.is_element_present(*ProductPageLocators.MESSAGE_BASKET_TOTAL), (
--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,20 @@
+import pytest
+
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_cart(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('offer_n', [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail), 8, 9])
+def test_guest_can_add_product_to_basket(browser, offer_n):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{offer_n}"
     page = ProductPage(browser, link)
     page.open()
     page.press_button_add_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_message_about_adding()
     page.should_be_message_basket_total()
+
+
+'''
+Запуск теста выполняется следующей командой:
+pytest -s test_product_page.py
+'''
